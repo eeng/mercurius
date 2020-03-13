3da3ba8b0241ab42ce6b3733b3ac8851bcf050fb
Emmanuel Nicolau <emmanicolau@gmail.com>
2020-03-13 13:53:22 -0300

    Add timbre logging library

--- src/mercurius/system.clj
+++ src/mercurius/system.clj
@@ -1,10 +1,11 @@
 (ns mercurius.system
   (:require [integrant.core :as ig]
+            [taoensso.timbre :as log]
             [mercurius.wallets.adapters.repositories.in-memory-wallet-repository :refer [make-in-memory-wallet-repo]]))
 
 (def config
   {:wallets/repository {}})
 
 (defmethod ig/init-key :wallets/repository [_ _]
-  (println "Starting InMemory")
+  (log/info "Using InMemoryWalletRepository")
   (make-in-memory-wallet-repo))
