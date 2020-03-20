7983dd35b942ca2819377f11a01e21984b4d35bc
Emmanuel Nicolau <emmanicolau@gmail.com>
2020-03-20 09:49:51 -0300

    The thrown-with-data is no longer needed

--- test/mercurius/wallets/domain/entities/wallet_test.clj
+++ test/mercurius/wallets/domain/entities/wallet_test.clj
@@ -1,7 +1,6 @@
 (ns mercurius.wallets.domain.entities.wallet-test
   (:require [clojure.test :refer [deftest testing is]]
             [matcher-combinators.test]
-            [mercurius.support.asserts]
             [mercurius.support.factory :refer [build-wallet]]
             [mercurius.wallets.domain.entities.wallet :refer [deposit withdraw reserve transfer]]))
 
@@ -12,10 +11,10 @@
       (is (= 150 (:balance new-wallet)))))
 
   (testing "amount should be positive"
-    (is (thrown-with-data? {:type :wallet/invalid-amount}
-                           (deposit {:balance 0} -1)))
-    (is (thrown-with-data? {:type :wallet/invalid-amount}
-                           (deposit {:balance 0} 0)))))
+    (is (thrown-match? clojure.lang.ExceptionInfo {:type :wallet/invalid-amount}
+                       (deposit {:balance 0} -1)))
+    (is (thrown-match? clojure.lang.ExceptionInfo {:type :wallet/invalid-amount}
+                       (deposit {:balance 0} 0)))))
 
 (deftest withdraw-test
   (testing "should decrease the wallet balance by the amount passed"
@@ -23,27 +22,27 @@
     (is (= 0 (-> (build-wallet {:balance 30}) (withdraw 30) :balance))))
 
   (testing "amount should be greater or equal to the available balance"
-    (is (thrown-with-data? {:type :wallet/insufficient-balance}
-                           (-> (build-wallet {:balance 30}) (withdraw 31))))
-    (is (thrown-with-data? {:type :wallet/insufficient-balance}
-                           (-> (build-wallet {:balance 30 :reserved 20}) (withdraw 11)))))
+    (is (thrown-match? clojure.lang.ExceptionInfo {:type :wallet/insufficient-balance}
+                       (-> (build-wallet {:balance 30}) (withdraw 31))))
+    (is (thrown-match? clojure.lang.ExceptionInfo {:type :wallet/insufficient-balance}
+                       (-> (build-wallet {:balance 30 :reserved 20}) (withdraw 11)))))
 
   (testing "amount should be positive"
     (let [wallet (build-wallet {:balance 5})]
-      (is (thrown-with-data? {:type :wallet/invalid-amount}
-                             (withdraw wallet -1))))))
+      (is (thrown-match? clojure.lang.ExceptionInfo {:type :wallet/invalid-amount}
+                         (withdraw wallet -1))))))
 
 (deftest reserve-test
   (testing "should increase the wallet reserved amount by the amount passed"
     (is (= 30 (-> (build-wallet {:balance 30}) (reserve 10) (reserve 20) :reserved))))
 
   (testing "amount should be positive"
-    (is (thrown-with-data? {:type :wallet/invalid-amount}
-                           (reserve (build-wallet) -1))))
+    (is (thrown-match? clojure.lang.ExceptionInfo {:type :wallet/invalid-amount}
+                       (reserve (build-wallet) -1))))
 
   (testing "amount must not be greater than the available balance"
-    (is (thrown-with-data? {:type :wallet/insufficient-balance}
-                           (reserve (build-wallet) 1)))))
+    (is (thrown-match? clojure.lang.ExceptionInfo {:type :wallet/insufficient-balance}
+                       (reserve (build-wallet) 1)))))
 
 (deftest transfer-test
   (testing "should transfer the amount from the first wallet to the second one"
@@ -55,11 +54,12 @@
   (testing "is not possible if the src wallet doesn't have enough balance"
     (let [src (build-wallet {:balance 0})
           dst (build-wallet {:balance 0})]
-      (is (thrown-with-data? {:type :wallet/insufficient-balance}
-                             (transfer src dst 1)))))
+      (is (thrown-match? clojure.lang.ExceptionInfo {:type :wallet/insufficient-balance}
+                         (transfer src dst 1)))))
 
   (testing "can't transfer between different currencies"
     (let [src (build-wallet {:balance 5 :currency "USD"})
           dst (build-wallet {:balance 0 :currency "BTC"})]
-      (is (thrown-with-data? {:type :wallet/different-currencies}
-                             (transfer src dst 1))))))
+      (is (thrown-match? clojure.lang.ExceptionInfo
+                         {:type :wallet/different-currencies}
+                         (transfer src dst 1))))))
