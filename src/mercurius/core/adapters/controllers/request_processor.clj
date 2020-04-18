8b66b61fe08f1cbafd66981431f5a80173cb2e63
Emmanuel Nicolau <emmanicolau@gmail.com>
2020-04-18 13:55:43 -0300

    Refactor the exception logging to the middleware

--- src/mercurius/core/adapters/controllers/request_processor.clj
+++ src/mercurius/core/adapters/controllers/request_processor.clj
@@ -1,17 +1,15 @@
 (ns mercurius.core.adapters.controllers.request-processor
   "Receives requests (commands and queries) coming from the UI
-  and dispatches them through the mediator."
-  (:require [taoensso.timbre :as log]))
+  and dispatches them through the mediator. 
+  Returns a tuple containing the status of the execution.")
 
 (defn new-request-processor [{:keys [dispatch]}]
   (fn [[request-type request-data] context]
     (try
       [:ok (dispatch request-type (merge request-data context))]
-      (catch clojure.lang.ExceptionInfo e
-        (log/error e)
-        [:error {:type (:type (ex-data e))
-                 :message (.getMessage e)}])
       (catch Exception e
-        (log/error e)
-        [:error {:type :unexpected-error
-                 :message (.getMessage e)}]))))
+        (if (instance? clojure.lang.ExceptionInfo e)
+          [:error {:type (:type (ex-data e))
+                   :message (.getMessage e)}]
+          [:error {:type :unexpected-error
+                   :message (.getMessage e)}])))))
--- src/mercurius/core/domain/use_cases/mediator/middleware/logger.clj
+++ src/mercurius/core/domain/use_cases/mediator/middleware/logger.clj
@@ -4,8 +4,12 @@
 (defn logger [next-handler]
   (fn [{:keys [type data] :as request}]
     (log/info "Executing" type data)
-    (let [start (System/currentTimeMillis)
-          result (next-handler request)
-          duration (- (System/currentTimeMillis) start)]
-      (log/info "Finished" type "in" duration "ms")
-      result)))
+    (try
+      (let [start (System/currentTimeMillis)
+            result (next-handler request)
+            duration (- (System/currentTimeMillis) start)]
+        (log/info "Finished" type "in" duration "ms")
+        result)
+      (catch Exception e
+        (log/error e)
+        (throw e)))))
